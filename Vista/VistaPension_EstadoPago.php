677abc8164b1a18b77fac068ac68d35b5742441a
Fernandd <edgar.fernandd.05@gmail.com>
2025-05-30 18:31:28 -0500

    Update VistaPension_EstadoPago.php
    dfgdfgd

--- Vista/VistaPension_EstadoPago.php
+++ Vista/VistaPension_EstadoPago.php
@@ -1,59 +1,85 @@
 <!DOCTYPE html>
+
 <html lang="es">
 
 <head>
-  <meta charset="UTF-8" />
-  <title>Validación de Pagos</title>
-  <link rel="stylesheet" href="../Vista/css/VistaPension_EstadoPago.css" />
+
+ <meta charset="UTF-8">
+
+ <title>Validación de Pagos</title>
+
+ <link rel="stylesheet" href="../ValidacionDatos.css">
+
 </head>
 
 <body>
-  <h1>Validación de Pagos</h1>
 
-  <div class="contenedor-principal"
-    
-    <form method="post">
-      <input type="number" placeholder="ID" name="id" required />
 
-      <select name="estado" required>
-        <option value="">Selecciona estado</option>
-        <option value="Activo">Activo</option>
-        <option value="Inactivo">Inactivo</option>
-      </select>
 
-      <input type="date" name="fecha" required />
+ <h1>Validación de Pagos</h1>
 
-      <input type="number" placeholder="Monto" name="monto" required />
 
-      <input type="datetime-local" name="fechaHoraOperacion" required />
 
-      <input type="number" placeholder="Número Operación" name="numeroOperacion" required />
+ <form method="post">
 
-      <input type="text" placeholder="Cuenta Destino" name="cuentaDestino" required />
-    
-    <a href="../Vista/VistaPension_Principal.php">
-      <button class="volver-btn" type="button">Volver al Menú Principal</button>
-    </a>
-      <button type="submit">Agregar</button>
-    </form>
+  <input type="number" placeholder="ID" name="id" required>
 
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Estado</th>
-          <th>Fecha</th>
-          <th>Monto</th>
-          <th>Fecha/Hora Operación</th>
-          <th>Número Operación</th>
-          <th>Cuenta Destino</th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- Aquí van las filas dinámicas -->
-      </tbody>
-    </table>
-  </div>
+  <select name="estado" required>
+
+   <option value="">Selecciona estado</option>
+
+   <option value="Activo">Activo</option>
+
+   <option value="Inactivo">Inactivo</option>
+
+  </select>
+
+  <input type="date" name="fecha" required>
+
+  <input type="number" placeholder="Monto" name="monto" required>
+
+  <input type="datetime-local" name="fechaHoraOperacion" required>
+
+  <input type="number" placeholder="Número Operación" name="numeroOperacion" required>
+
+  <input type="text" placeholder="Cuenta Destino" name="cuentaDestino" required>
+
+  <button type="submit">Agregar</button>
+
+ </form>
+
+
+
+ <table>
+
+  <thead>
+
+   <tr>
+
+    <th>ID</th>
+
+    <th>Estado</th>
+
+    <th>Fecha</th>
+
+    <th>Monto</th>
+
+    <th>Fecha/Hora Operación</th>
+
+    <th>Número Operación</th>
+
+    <th>Cuenta Destino</th>
+
+   </tr>
+
+  </thead>
+
+  </tbody>
+
+ </table>
+
+ 
+
 </body>
 
 </html>
