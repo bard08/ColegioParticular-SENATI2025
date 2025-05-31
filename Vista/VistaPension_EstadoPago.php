7176a45bc9acfd7a15f9f21eaa2dc2a174e7a62b
Marco Pizango <franv9473@gmail.com>
2025-05-30 21:09:47 -0500

    Update VistaPension_EstadoPago.php
    hjbnhrtyh

--- Vista/VistaPension_EstadoPago.php
+++ Vista/VistaPension_EstadoPago.php
@@ -6,7 +6,7 @@
 
  <meta charset="UTF-8">
 
- <title>Validación de Pagos</title>
+
 
  <link rel="stylesheet" href="../Vista/css/VistaPension_EstadoPago.css">
 
@@ -17,15 +17,13 @@
 
  <h1>Validación de Pagos</h1>
 
-<a href="../Vista/VistaPension_Principal.php">
-        <button class="volver-btn">Volver al Menú Principal</button>
-</a>
+
 
  <form method="post">
 
-  <input type="number" placeholder="ID" name="id" required>
+  <input type="number" placeholder="ID" name="id" >
 
-  <select name="estado" required>
+  <select name="estado" >
 
    <option value="">Selecciona estado</option>
 
@@ -35,17 +33,20 @@
 
   </select>
 
-  <input type="date" name="fecha" required>
+  <input type="date" name="fecha" >
 
-  <input type="number" placeholder="Monto" name="monto" required>
+  <input type="number" placeholder="Monto" name="monto" >
 
-  <input type="datetime-local" name="fechaHoraOperacion" required>
+  <input type="datetime-local" name="fechaHoraOperacion" >
 
-  <input type="number" placeholder="Número Operación" name="numeroOperacion" required>
+  <input type="number" placeholder="Número Operación" name="numeroOperacion" >
 
-  <input type="text" placeholder="Cuenta Destino" name="cuentaDestino" required>
+  <input type="text" placeholder="Cuenta Destino" name="cuentaDestino" >
 
   <button type="submit">Agregar</button>
+  <a href="../Vista/VistaPension_Principal.php">
+
+<button class="volver-btn" type="button">Volver al Menú Principal</button> </a>
 
  </form>
 
